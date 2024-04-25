de6e577cdf7e819a350205746a8c687ad061e969
deplagene <deplagene@mail.ru>
2024-04-25 18:18:21 +0300

    feat:add drink controller

--- DrinkDispenser.WebApi/Controllers/DrinksController.cs
+++ DrinkDispenser.WebApi/Controllers/DrinksController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using DrinkDispenser.Application.Services.DrinksService;
 using DrinkDispenser.Contracts.Drinks;
+using DrinkDispenser.Contracts.Drinks.Requests;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DrinkDispenser.WebApi.Controllers;
@@ -21,7 +22,74 @@ public class DrinksController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> CreateDrink([FromBody] DrinkRequest request, CancellationToken cancellationToken)
     {
-        var drink = await _drinkService.CreateDrink(request.Name, request.Price, request.ImageUrl, cancellationToken);
+        var drink = await _drinkService.CreateDrink(request.Name, request.Price, request.ImageUrl, request.VendingMachineId, cancellationToken);
+
+        if (drink.IsError)
+            return BadRequest(drink.Errors);
+
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetDrinks(CancellationToken cancellationToken)
+    {
+        var drinks = await _drinkService.GetAllDrinks(cancellationToken);
+
+        if (drinks.IsError)
+            return BadRequest(drinks.Errors);
+
+        return Ok(drinks.Value);
+    }
+
+    [HttpGet("available")]
+    public async Task<IActionResult> GetAvailableDrinks(CancellationToken cancellationToken)
+    {
+        var drinks = await _drinkService.GetAvailableDrinks(cancellationToken);
+
+        if (drinks.IsError)
+            return BadRequest(drinks.Errors);
+
+        return Ok(drinks.Value);
+
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetDrinkById(Guid id, CancellationToken cancellationToken)
+    {
+        var drink = await _drinkService.GetDrinkById(id, cancellationToken);
+
+        if (drink.IsError)
+            return BadRequest(drink.Errors);
+
+        return Ok(drink.Value);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteDrink(Guid id, CancellationToken cancellationToken)
+    {
+        var drink = await _drinkService.DeleteDrink(id, cancellationToken);
+
+        if (drink.IsError)
+            return BadRequest(drink.Errors);
+
+        return Ok();
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> UpdateDrink(Guid DrinkId,[FromBody] DrinkPatchRequest request, CancellationToken cancellationToken)
+    {
+        var drink = await _drinkService.UpdateDrink(DrinkId, request.Name, request.Price, request.ImageUrl, cancellationToken);
+
+        if (drink.IsError)
+            return BadRequest(drink.Errors);
+
+        return Ok();
+    }
+
+    [HttpPatch("{id}/availability")]
+    public async Task<IActionResult> UpdateDrinkAvailability(Guid drinkId, [FromBody] bool? isAvailable, CancellationToken cancellationToken)
+    {
+        var drink = await _drinkService.UpdateDrinkAvailability(drinkId, isAvailable, cancellationToken);
 
         if (drink.IsError)
             return BadRequest(drink.Errors);
--- DrinkDispenser.WebApi/obj/Debug/net8.0/DrinkDispenser.WebApi.AssemblyInfo.cs
+++ DrinkDispenser.WebApi/obj/Debug/net8.0/DrinkDispenser.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrinkDispenser.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00fb1dff0ac148fb313fe627a3b6da61caa727e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df3b9f83194973782e23b37a2c3eee1293113333")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrinkDispenser.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DrinkDispenser.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
