45c4c40aa568c34cc8b6769a78680e0aa5a01768
deplagene <deplagene@mail.ru>
2024-07-29 16:11:20 +0300

    feat: изменил структуру проекта

--- ui/variable.js
+++
@@ -1,8 +0,0 @@
-const variables={
-    API_URL: "http://localhost:5259/api/",
-    DRINKS_URL: "http://localhost:5259/api/drinks/",
-    USERS_URL: "http://localhost:5259/api/users/",
-    LOGIN_URL: "http://localhost:5259/api/login/",
-    REGISTER_URL: "http://localhost:5259/api/register/",
-    VENDING_MACHINES_URL: "http://localhost:5259/api/vendingmachines/"
-};
